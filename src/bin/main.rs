a6bc71d21120fc34aad428db819e845cb5e233c3
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-06-05 14:07:31 +0800

    Single file readme #28 (#30)

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -26,31 +26,42 @@ fn main() {
         None => PathBuf::from("hanji-out"),
     };
 
-    if in_path.is_dir() {
-        remove_dir_all(&out_path).unwrap();
-        create_dir(&out_path).unwrap();
-        let cairo_files = get_cairo_files_in_path(&in_path);
-        for cairo_file in cairo_files.iter() {
-            let rel_path = cairo_file.strip_prefix(&in_path).unwrap();
-            let mut doc_file_path = out_path.clone();
-            doc_file_path.push(rel_path);
-            doc_file_path.set_extension("md");
-
-            create_dir_all(&doc_file_path.parent().unwrap()).unwrap();
-
-            let mut file = File::create(&doc_file_path.into_os_string().to_str().unwrap()).unwrap();
-
-            let docs = print_markdown(cairo_file.to_str().unwrap());
-            file.write_all(docs.as_bytes()).unwrap();
+    // Clean out_path
+    if out_path.exists() {
+        println!("Clearing out_path {:?}", out_path);
+        match remove_dir_all(&out_path) {
+            Err(_) => {
+                println!("Clearing out_path file {:?}", out_path);
+                remove_file(&out_path).unwrap();
+            }
+            _ => {}
         }
-    } else {
-        print_cairo_file_docs(&args[1]);
     }
 
-    // Cairo language spec
-    // cairo_lang_syntax_codegen::cairo_spec::get_spec
+    if in_path.is_dir() {
+        create_dir_all(&out_path).unwrap();
+        let cairo_files = get_cairo_files_in_path(&in_path);
+        for cairo_file in cairo_files.iter() {
+            handle_md_file(cairo_file, &in_path, &out_path);
+        }
+    } else {
+        handle_md_file(&in_path, &in_path, &out_path);
+    }
 }
 
-fn print_cairo_file_docs(cairo_filename: &str) {
-    println!("{}", print_markdown(cairo_filename));
+fn handle_md_file(cairo_file: &PathBuf, in_path: &PathBuf, out_path: &PathBuf) {
+    let rel_path = cairo_file.strip_prefix(in_path).unwrap();
+    let mut doc_file_path = out_path.clone();
+
+    if doc_file_path.is_dir() {
+        doc_file_path.push(rel_path);
+    }
+    doc_file_path.set_extension("md");
+
+    create_dir_all(&doc_file_path.parent().unwrap()).unwrap();
+
+    let mut file = File::create(&doc_file_path.into_os_string().to_str().unwrap()).unwrap();
+
+    let docs = print_markdown(cairo_file.to_str().unwrap());
+    file.write_all(docs.as_bytes()).unwrap();
 }
